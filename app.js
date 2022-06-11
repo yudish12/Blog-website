87030059bcd681726ee2440aabcc025674aa515e
yudish <yudishchakrawarty3042@gmail.com>
2022-06-11 18:51:03 +0530

    sad

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/public'));
 app.get("/",(req,res)=>{
 
 res.render("home",{route:"Home",homeContent:homeContent,postArr:postArr});
-console.log(postArr);
+
 });
 
 app.get("/about",(req,res)=>{
