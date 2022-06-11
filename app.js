55969fc52469e8d86f44c07ca3f2e4d2879530dd
yudish <yudishchakrawarty3042@gmail.com>
2022-06-11 16:03:16 +0530

    blog

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { route } = require("express/lib/application");
+const app = express();
+const homeContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi gravida libero nec velit. Morbi scelerisque luctus velit. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Proin mattis lacinia justo. Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque libero. Maecenas libero. Etiam dictum tincidunt diam. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec, pretium sit amet, tempor quis, urna. Aenean placerat. In vulputate urna eu arcu. Aliquam erat volutpat. Suspendisse potenti. Morbi mattis felis at nunc. Duis viverra diam non justo. In nisl. Nullam sit amet magna in magna gravida vehicula. Mauris tincidunt sem sed arcu. Nunc posuere. Nullam lectus justo, vulputate eget, mollis sed, tempor sed, magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam neque. Curabitur ligula sapien, pulvinar a, vestibulum quis, facilisis vel, sapien. Nullam eget nisl. Donec vitae arci"
+
+const AboutContent  =  "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+
+const postArr = [];
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",(req,res)=>{
+
+res.render("home",{route:"Home",homeContent:homeContent,postArr:postArr});
+console.log(postArr);
+});
+
+app.get("/about",(req,res)=>{
+
+    res.render("about",{route:"About",aboutContent:AboutContent});
+});
+
+app.get("/Contact",(req,res)=>{
+
+    res.render("contact",{route:"Contact"});
+});
+
+app.get("/Compose",(req,res)=>{
+ 
+    res.render("compose",{route:"Compose-Journal"});
+});
+
+app.post("/Contact",(req,res)=>{
+
+});
+
+app.post("/Compose",(req,res)=>{
+let post = {
+    title:req.body.title,
+    content:req.body.content
+};
+postArr.push(post);
+res.redirect("/");
+});
+
+app.listen(process.env.PORT || 8000,()=>{
+    console.log("server started on port 8000");
+});
+    
